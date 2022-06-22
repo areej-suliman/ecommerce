7f4f8c259d849600b513a19b5bd03c528306d139
arooja <arooja.ao@gmail.com>
2022-06-22 16:31:54 +0300

    carousel feature

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import 'popper.js/dist/popper.min.js';
 import '@fortawesome/fontawesome-free/js/all.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
+
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
@@ -88,6 +92,22 @@ $(function () {
 
       $('.credit-card-info').toggle();
     });
+
+
+    $( function() {
+      $( "#price-range" ).slider({
+        range: true,
+        min: 50,
+        max: 1000,
+        step: 50,
+        values: [250, 800],
+        slide: function( event, ui ) {
+          $("#price-min").text( ui.values[0]);
+          $("#price-max").text( ui.values[1]);
+        }
+      });
+    
+    } );
 
   });
 
